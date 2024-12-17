9eb2dfe19d00fb54484e264810c0ab2cf3b62b89
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-12-17 05:11:28 +0400

    crash logs was added

--- android/src/main/java/net/warsmash/phone/android/engine/activity/EngineActivity.kt
+++ android/src/main/java/net/warsmash/phone/android/engine/activity/EngineActivity.kt
@@ -3,23 +3,25 @@ package net.warsmash.phone.android.engine.activity
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Environment
 import android.system.Os
 import androidx.preference.PreferenceManager
 import barsoosayque.libgdxoboe.OboeAudio
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import com.badlogic.gdx.backends.android.AndroidAudio
 import com.etheller.warsmash.WarsmashGdxMenuScreen
 import com.etheller.warsmash.WarsmashGdxMultiScreenGame
-import net.warsmash.phone.R
 import net.warsmash.phone.android.engine.loadExtensions
 import net.warsmash.phone.utils.GAME_FILES_SHARED_PREFS_KEY
 import net.warsmash.phone.utils.extensions.displayInCutoutArea
+import java.io.File
 import java.util.function.Consumer
+
 
 /** Launches the Android application.  */
 class EngineActivity : AndroidApplication() {
+    private lateinit var loggerProcess : Process
     private lateinit var prefsManager : SharedPreferences
 
     override fun createAudio(context: Context, config: AndroidApplicationConfiguration): AndroidAudio =
@@ -28,6 +30,8 @@ class EngineActivity : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        loggerProcess = createProcess(Environment.getExternalStorageDirectory().absolutePath
+                + "/" + "warsmash.log")
         prefsManager = PreferenceManager.getDefaultSharedPreferences(this)
         Os.setenv(GAME_FILES_SHARED_PREFS_KEY,
             prefsManager.getString(GAME_FILES_SHARED_PREFS_KEY,""), true)
@@ -47,8 +51,24 @@ class EngineActivity : AndroidApplication() {
         initialize(warsmashGdxMultiScreenGame, configuration)
     }
 
+    public override fun onDestroy() {
+        super.onDestroy()
+        loggerProcess.destroy()
+    }
+
     private fun getGameVersion () : Int {
         val gameVersion = prefsManager.getString("Game version", "");
         return if (gameVersion == "TFT") 1 else 0
+    }
+
+    private fun createProcess(pathToLog: String): Process {
+        val file = File(pathToLog)
+        if (file.exists()){
+            file.delete()
+        }
+        val processBuilder = ProcessBuilder()
+        processBuilder.command("/system/bin/sh", "-c", "logcat *:W -d -f $pathToLog")
+        processBuilder.redirectErrorStream(true)
+        return processBuilder.start()
     }
 }
